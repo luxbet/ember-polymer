40e53c5e93c215e388d8a07b084c56202210c4d3
Steven Ferguson <steven.ferguson11@gmail.com>
2015-04-22 14:33:34 -0700

    Use new addon API

--- index.js
+++ index.js
@@ -2,44 +2,45 @@
 // 'use strict';
 
 var vulcanize = require('broccoli-vulcanize');
-var Funnel = require('broccoli-funnel');
-var mergeTrees  = require('mergeTrees');
+var funnel = require('broccoli-funnel');
+var mergeTrees  = require('broccoli-merge-trees');
 
 module.exports = {
   name: 'ember-polymer',
 
-  contentFor: function contentFor(type, config) {
+  contentFor: function(type, config) {
     if (type === 'head-footer') {
       return [
-        '<script src="public/webcomponents-lite.js"></script>',
+        '<script src="webcomponents.js"></script>',
         '<link rel="import" href="elements.html">'
       ];
     }
   },
-  
-  treeForApp: function treeForPublic(tree) {
+
+  treeForPublic: function(tree) {
+    var webcomponents = funnel('bower_components/webcomponentsjs', {
+      srcDir: '/',
+      files: ['webcomponents.js'],
+      destDir: '/'
+    });
+
     var vulcanized = vulcanize('app', {
       input: 'elements.html',
       csp: true,
       inline: true,
-      strip: !this.isDevelopingAddon(),
+      strip: true,
       excludes: {
         imports: ["(^data:)|(^http[s]?:)|(^\/)"],
         scripts: ["(^data:)|(^http[s]?:)|(^\/)"],
         styles: ["(^data:)|(^http[s]?:)|(^\/)"]
       }
-    );
-    
-    return mergeTrees([tree, vulcanized]);
-  },
-  
-  treeForPublic: function treeForPublic(tree) {
-    var webcomponents = new Funnel('bower_components/webcomponentsjs', {
-      srcDir: '/',
-      include: ['webcomponents-lite.js'],
-      destDir: '/'
     });
-    
-    return mergeTrees([tree, webcomponents]);
+
+    var trees = [webcomponents, vulcanized];
+    if (tree) {
+      trees.unshift(tree);
+    }
+
+    return mergeTrees(trees);
   }
 };
